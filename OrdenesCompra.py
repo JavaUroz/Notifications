d48b75dd7609c312810c45f721f23f559a9d19a5
JavaUroz <urozjavier@gmail.com>
2024-04-05 14:48:09 -0300

    OrdenesCompra

---
+++ OrdenesCompra.py
@@ -0,0 +1,273 @@
+#-*- coding: utf-8 -*-
+
+import decimal
+from math import radians
+import os
+from tokenize import Double
+from xml.dom.minidom import TypeInfo
+from xmlrpc.client import DateTime
+from twilio.rest import Client, content
+import pyodbc
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import dotenv
+from datetime import datetime
+from datetime import timedelta
+from dateutil.relativedelta import relativedelta
+
+# Cargar librería para .env
+dotenv.load_dotenv()
+
+# Establecer el connection string
+connection_string = "Driver={SQL Server};Server=SERVIDOR;Database=SBDACEST;UID=sa;PWD=Sa2008R2;"
+
+# Establecer la consulta SQL
+sql_query = """
+                SELECT 
+                    [SegCabC].[sccpro_Cod],
+                    [SegCabC].[sccpro_RazSoc],
+                    [SegTiposC].[spctco_Cod],
+                    [SegTiposC].[spc_Nro],
+                    [SegDetC].[sdc_FRecep]
+                FROM [SBDACEST].[dbo].[SegTiposC]
+                INNER JOIN [SegDetC] ON [SegTiposC].[spcscc_ID] = [SegDetC].[sdcscc_ID]
+                INNER JOIN [SegCabC] ON [SegTiposC].[spcscc_ID] = [SegCabC].[scc_ID]
+                WHERE [sdc_TipoIt] != 'L' AND 
+                    [spctco_Cod] != 'PC' AND 
+                    ([sdc_CPendRtUM1] > 0 OR [sdc_CPendRtUM2] > 0) AND 
+                    [spc_Nro] > 0 AND 
+                    ([sdccon_Cod] IN ('015','015A','033','112','117','024','025') OR [sdcart_CodGen] > 0) AND 
+                    ([spctco_Cod] IN ('OC','OCP','OCR') OR 
+                        ([spctco_Cod] = 'FC' AND [sccpro_Cod] IN ('008790', '010406'))
+                    ) AND 
+                    ([sdc_Desc] NOT IN ('Materiales para Fabricaci\u00f3n',
+                                        'Materiales para Fabricaci\u00f3n (IVA 10,5%)',
+                                        'Repuestos y Reparaciones (IVA 21%)',
+                                        'Ferias y Exposiciones (IVA 21%)',
+                                        'Materiales para la construcci\u00f3n',
+                                        'Ferreter\u00EDa - Art\u00EDculos varios',
+                                        'Muebles y Utiles') AND    
+                     ([sdc_Desc] NOT LIKE '%Materiales para Fabricaci\u00f3n%' AND
+                      [sdc_Desc] NOT LIKE '%Repuestos y Reparaciones%' AND
+                      [sdc_Desc] NOT LIKE '%Ferias y Exposiciones%' AND
+                      [sdc_Desc] NOT LIKE '%Materiales para la construcci\u00f3n%' AND
+                      [sdc_Desc] NOT LIKE '%Ferreter\u00EDa - Art\u00EDculos varios%' AND
+                      [sdc_Desc] NOT LIKE '%Muebles y Utiles%')
+                    ) 	
+                GROUP BY [SegDetC].[sdc_FRecep],
+                    [SegCabC].[sccpro_Cod],
+                    [SegCabC].[sccpro_RazSoc],
+                    [SegTiposC].[spctco_Cod],
+                    [SegTiposC].[spc_Nro]
+                ORDER BY [SegDetC].[sdc_FRecep];
+"""
+
+# Establecer cliente con credenciales de SID y Token de Twilio 
+account_sid = os.environ['ACCOUNT_SID']
+auth_token = os.environ['AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
+mensaje_plantilla = ''
+i = 1
+indices_acumulados = []
+
+contenido_html = """
+<html>
+<head>
+  <style>
+    table {
+      border-collapse: collapse;
+      width: 100%;
+    }
+    th, td {
+      border: 1px solid black;
+      padding: 8px;
+      text-align: left;
+    }
+    .atrasado-container{
+       background-color: red;
+    }
+    .atrasado{
+       color: white;
+       font-style:italic;
+    }
+    .entrega-semana-container{
+       background-color: yellow;
+    }
+    .entrega-semana{
+       color: black;
+    }
+    .entrega-quincena-container{
+       background-color: green;
+    }
+    .entrega-quincena{
+       color: white;
+    }
+  </style>
+</head>
+<body>
+  <h2>Entregas pendientes</h2>
+  <table>
+    <tr>
+      <th>Empresa</th>
+      <th>Orden Compras</th>
+      <th>Entrega</th>
+      <th>Estado</th>
+    </tr>
+"""
+
+# Función para enviar mensaje de WhatsApp
+def enviar_mensaje_whatsapp(destinatario, mensaje):
+    # Split the message into chunks of 1600 characters or less
+    message_chunks = [mensaje[i:i+1600] for i in range(0, len(mensaje), 1600)]
+    
+   
+    
+    
+    for contador, chunk in enumerate(message_chunks):                
+        try:
+            message = client.messages.create(
+                                      from_='whatsapp:+14155238886',
+                                      body = chunk,                              
+                                      to=f'whatsapp:{destinatario}'
+                                  )
+            print('Mensaje enviado correctamente:', message.sid)
+            if contador >= 1:
+                message = client.messages.create(
+                                      from_='whatsapp:+14155238886',
+                                      body = f'Se excedio la cantidad de mensajes permitidos({contador + 1})\nEl cuadro completo se enviara a las casillas de correo asignadas.',                              
+                                      to=f'whatsapp:{destinatario}'
+                                  )
+                print(f'Se alcanzo el l\u00EDmite de mensajes permitidos de ({len(message_chunks) - 1}):\n')
+                break                
+        except Exception as e:
+            print(f'Ha ocurrido un error:\n', e)
+
+# Inicia la conexión
+try:
+    # Establecer la conexión con la base de datos
+    conexion = pyodbc.connect(connection_string)
+
+    # Crear un cursor para ejecutar la consulta SQL
+    cursor = conexion.cursor()
+
+    # Ejecutar la consulta SQL
+    cursor.execute(sql_query)
+
+    # Obtener los resultados
+    resultados = cursor.fetchall()
+
+    # Procesar los resultados
+    for resultado in resultados:
+        codProveedor = resultado[0]
+        razonSocial = resultado[1]
+        tipoComprobante = resultado[2]
+        numeroComprobante = resultado[3]        
+        fechaEntrega = resultado[4]
+        
+        fechaActual = datetime.now()
+        diferencia =  fechaEntrega - fechaActual
+        diferenciaDias = diferencia.days
+        tiempoLimite = 15
+        if diferenciaDias < tiempoLimite:        
+            if diferenciaDias < 0:            
+                contenido_html += f"""
+                    <tr>
+                      <td>({codProveedor}) {razonSocial}</td>
+                      <td>{tipoComprobante} {int(numeroComprobante)}</td>
+                      <td>{fechaEntrega.strftime('%d/%m/%Y')}</td>                  
+                      <td class="atrasado-container"><span class="atrasado"><b>Atrasado por {diferenciaDias} d\u00EDas</b></td>
+                    </tr>
+                """
+                mensaje_plantilla += f'{i}) *{int(codProveedor)} {razonSocial[:25]}* {fechaEntrega.strftime('%d/%m/%Y')} - Atrasado por *{diferenciaDias*-1}* d\u00EDas\n'
+        
+            elif diferenciaDias > 0 and diferenciaDias <= 7:            
+                contenido_html += f"""
+                    <tr>
+                      <td>({codProveedor}) {razonSocial}</td>
+                      <td>{tipoComprobante} {int(numeroComprobante)}</td>
+                      <td>{fechaEntrega.strftime('%d/%m/%Y')}</td>                  
+                      <td class="entrega-semana-container"><span class="entrega-semana"><b>Entrega en {diferenciaDias} d\u00EDas</b></span></td>
+                    </tr>
+                """
+                mensaje_plantilla += f'{i}) *{int(codProveedor)} {razonSocial[:25]}* {fechaEntrega.strftime('%d/%m/%Y')} - Entrega en *{diferenciaDias}* d\u00EDas\n'
+            
+            elif diferenciaDias > 7 and diferenciaDias <= 15:            
+                contenido_html += f"""
+                    <tr>
+                      <td>({codProveedor}) {razonSocial}</td>
+                      <td>{tipoComprobante} {int(numeroComprobante)}</td>
+                      <td>{fechaEntrega.strftime('%d/%m/%Y')}</td>                  
+                      <td class="entrega-quincena-container"><span class="entrega-quincena"><b>Entrega en {diferenciaDias} d\u00EDas</b></span></td>
+                    </tr>
+                """
+                mensaje_plantilla += f'{i}) *{int(codProveedor)} {razonSocial[:25]}* {fechaEntrega.strftime('%d/%m/%Y')} - Entrega en *{diferenciaDias}* d\u00EDas\n'
+            
+            else:
+                contenido_html += f"""
+                    <tr>
+                      <td>({codProveedor}) {razonSocial}</td>
+                      <td>{tipoComprobante} {int(numeroComprobante)}</td>
+                      <td>{fechaEntrega.strftime('%d/%m/%Y')}</td>                  
+                      <td class="entrega-semana-container"><span class="entrega-semana"><b>Entrega para hoy</b></span></td>
+                    </tr>
+                """
+                mensaje_plantilla += f'{i}) *{int(codProveedor)} {razonSocial[:25]}* {fechaEntrega.strftime('%d/%m/%Y')} - *Entrega para hoy!*\n'
+        i += 1
+            
+    mensaje_completo = f'PENDIENTES A RECLAMAR:\n   -    RAZ\u00D3N SOCIAL    -   F. ENTREGA   -   SITUACI\u00D3N   -\n{mensaje_plantilla}'
+    
+    contenido_html += """
+      </table>
+    </body>
+    </html>
+    """
+    
+    len_mensaje = len(mensaje_completo)
+    
+    try:    
+        # Llamar a la función para enviar el mensaje Javier Uroz
+        enviar_mensaje_whatsapp('+5492473501336', mensaje_completo)
+    except Exception as e:
+        print('Error al enviar mensaje: \n',e)
+        
+    # Prueba en cmd
+    print(mensaje_completo)
+
+except pyodbc.Error as e:
+    print('Ocurrio un error al conectar a la base de datos:', e)
+
+remitente = 'javieruroz@imcestari.com'
+# destinatario  = ['javieruroz@imcestari.com', 'mcelli@imcestari.com']
+destinatario  = ['javieruroz@imcestari.com']
+asunto = 'Pendientes de recibir - RECLAMAR'
+msg = contenido_html
+
+mensaje = MIMEMultipart()
+
+mensaje['From'] = remitente
+mensaje['To'] = ", ".join(destinatario)
+mensaje['Subject'] = asunto
+
+mensaje.attach(MIMEText(contenido_html, 'html'))
+
+# Datos
+username = os.environ['USERNAME']
+password = os.environ['PASSWORD']
+
+# Enviando el correo
+server = smtplib.SMTP_SSL('px000056.ferozo.com:465')
+# server.starttls()
+username=remitente
+server.login(username,password)
+try:
+    server.sendmail(remitente, destinatario, mensaje.as_string())
+    server.quit()
+    print('E- mail enviado exitosamente!')
+except Exception as e:
+    print('Ha ocurrido un error:\n', e)
+
+# Cerrar el cursor y la conexión
+cursor.close()
+conexion.close()
---
+++ SaldosLimites.py
@@ -0,0 +1,243 @@
+# -*- coding: utf-8 -*-
+
+import decimal
+import os
+from tokenize import Double
+from twilio.rest import Client, content
+import pyodbc
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import dotenv
+
+# Cargar librería para .env
+dotenv.load_dotenv()
+
+# Establecer el connection string
+connection_string = "Driver={SQL Server};Server=SERVIDOR;Database=SBDACEST;UID=sa;PWD=Sa2008R2;"
+
+# Establecer la consulta SQL
+sql_query = """
+SELECT ccopro_CUIT, ccopro_RazSoc, pro_CodPos, pro_Loc, SUM(cco_ImpMonLoc) AS TotalImpMonLoc
+FROM dbo.QRY_COMPRASPAGOS
+WHERE DescTipCond != 'CONTADO'
+GROUP BY ccopro_CUIT, ccopro_RazSoc, pro_CodPos, pro_Loc
+ORDER BY pro_CodPos, TotalImpMonLoc
+"""
+
+# Establecer cliente con credenciales de SID y Token de Twilio 
+account_sid = os.environ['ACCOUNT_SID']
+auth_token = os.environ['AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
+mensaje_plantilla = ''
+i = 1
+indices_acumulados = []
+
+contenido_html = """
+<html>
+<head>
+  <style>
+    table {
+      border-collapse: collapse;
+      width: 100%;
+    }
+    th, td {
+      border: 1px solid black;
+      padding: 8px;
+      text-align: left;
+    }
+    .saldo{
+       color: red;
+    }
+  </style>
+</head>
+<body>
+  <h2>Estado de cuentas</h2>
+  <table>
+    <tr>
+      <th>Empresa</th>
+      <th>CUIT</th>
+      <th>Saldo</th>
+    </tr>
+"""
+
+# Función para enviar mensaje de WhatsApp
+def enviar_mensaje_whatsapp(destinatario, mensaje):
+    # Split the message into chunks of 1600 characters or less
+    message_chunks = [mensaje[i:i+1600] for i in range(0, len(mensaje), 1600)]
+    
+   
+    
+    
+    for contador, chunk in enumerate(message_chunks):                
+        try:
+            message = client.messages.create(
+                                      from_='whatsapp:+14155238886',
+                                      body = chunk,                              
+                                      to=f'whatsapp:{destinatario}'
+                                  )
+            print('Mensaje enviado correctamente:', message.sid)
+            if contador >= 5:
+                message = client.messages.create(
+                                      from_='whatsapp:+14155238886',
+                                      body = f'Se excedio la cantidad de mensajes permitidos({contador + 1})\nEl cuadro completo se enviara a las casillas de correo asignadas.',                              
+                                      to=f'whatsapp:{destinatario}'
+                                  )
+                print(f'Se alcanzo el limite de mensajes permitidos de ({len(message_chunks) - 1}):\n')
+                break                
+        except Exception as e:
+            print(f'Ha ocurrido un error:\n', e)
+
+# Inicia la conexión
+try:
+    # Establecer la conexión con la base de datos
+    conexion = pyodbc.connect(connection_string)
+
+    # Crear un cursor para ejecutar la consulta SQL
+    cursor = conexion.cursor()
+
+    # Ejecutar la consulta SQL
+    cursor.execute(sql_query)
+
+    # Obtener los resultados
+    resultados = cursor.fetchall()
+
+    # Procesar los resultados
+    for resultado in resultados:        
+        cuit = resultado[0]
+        cuit_formateado = cuit[:2] + "-" + cuit[2:10] + "-" + cuit[10:]
+        razon_social = resultado[1]
+        codigo_postal = resultado[2]
+        if codigo_postal == None:
+            codigo_postal = 'None'
+        localidad = resultado[3]
+        if localidad == None:
+            localidad = 'None'
+        localidad_formateado = localidad.upper()
+        saldo = resultado[4]        
+        limite = -60000000
+        diferencia = saldo - limite
+        
+        if 'banco' in razon_social.lower():
+            limite = -100000000
+        elif '2720' in codigo_postal.lower() and not \
+            ('20221493185' in cuit.lower()) and not \
+            ('30506730038' in cuit.lower()) and not \
+            ('30717013162' in cuit.lower()) and not \
+            ('20132864935' in cuit.lower()) and not \
+            ('30707067434' in cuit.lower()) and not \
+            (razon_social == 'GAS PIC S.A.') and not \
+            ('pardo' in razon_social.lower()):
+            limite = -5000;
+        elif 'graneros' in razon_social.lower():
+            limite = -150000
+        elif (('transporte' in razon_social.lower()) and (cuit != '30710824106') and (cuit != '30516892788')) or \
+            ('damario' in razon_social.lower()) or \
+            ('comision' in razon_social.lower()) or \
+            ('bayl' in razon_social.lower()) or \
+            ('30714693243' in cuit.lower()) or \
+            ('30515999686' in cuit.lower()) or \
+            ('20319059394' in cuit.lower()) or \
+            ('30707735577' in cuit.lower()) or \
+            ('20046963335' in cuit.lower()) or \
+            ('20303127411' in cuit.lower()) or \
+            ('20362238650' in cuit.lower()) or \
+            ('20103107572' in cuit.lower()) or \
+            ('bossolani' in razon_social.lower()) or \
+            (('cargo' in razon_social.lower()) and (cuit != '30710188501') and (cuit != '20221493185')): # Transportes exceptuando Transporte Patron y Transporte Italia
+            limite = -10000
+        elif ('rosatto' in razon_social.lower()) or ('cr comisiones' in razon_social.lower()):
+            limite = -300000
+        elif 'gas pic s.a. (combustibles)' in razon_social.lower():
+            limite = -1000000
+        elif '30711186154' in cuit: # Solser
+            limite = -500000
+        elif '33583193109' in cuit: # Plegamex
+            limite = -50000
+        elif '23067223284' in cuit: # Oxitécnica
+            limite = -150000
+        elif 'gas pic s.a. (lubricantes)' in razon_social.lower():
+            limite = -100000        
+    
+        # Comprueba si el saldo supera el límite de la cuenta y ejecura la función verificar_saldos_y_enviar_notificaciones
+        if saldo < limite:
+            if '2720' in codigo_postal.lower():
+                contenido_html += f"""
+                    <tr>
+                      <td><b>({localidad_formateado})</b> {razon_social}</td>
+                      <td>{cuit_formateado}</td>
+                      <td><b><span class="saldo">${round(saldo, 2)}</span></td>
+                    </tr>
+                """
+                mensaje_plantilla += f'{i}) *({localidad_formateado})* {razon_social} - {cuit_formateado} - *${round(saldo, 2)}*\n'
+            # Mensaje de la plantilla
+            else:
+                contenido_html += f"""
+                    <tr>
+                      <td>{razon_social}</td>
+                      <td>{cuit_formateado}</td>
+                      <td><b><span class="saldo">${round(saldo, 2)}</span></td>
+                    </tr>
+                """
+                mensaje_plantilla += f'{i}) {razon_social} - {cuit_formateado} - *${round(saldo, 2)}*\n'
+            i += 1
+            
+    mensaje_completo = f'ATENCION PAGOS EXCEDIDOS: \n   -    RAZON SOCIAL    -   CUIT   -   SALDO   -\n{mensaje_plantilla}'
+    
+    contenido_html += """
+      </table>
+    </body>
+    </html>
+    """
+    
+    len_mensaje = len(mensaje_completo)
+    
+    try:        
+        # Llamar a la función para enviar el mensaje a Javier Gabarini
+        enviar_mensaje_whatsapp('+5492473504073', mensaje_completo)
+    
+        # Llamar a la función para enviar el mensaje Javier Uroz
+        enviar_mensaje_whatsapp('+5492473501336', mensaje_completo)
+    except Exception as e:
+        print('Error al enviar mensaje: \n',e)
+        
+    # Prueba en cmd
+    print(mensaje_completo)
+
+except pyodbc.Error as e:
+    print('Ocurrio un error al conectar a la base de datos:', e)
+
+remitente = 'javieruroz@imcestari.com'
+destinatario  = ['javieruroz@imcestari.com', 'jgabarini@imcestari.com']
+# destinatario  = ['javieruroz@imcestari.com']
+asunto = 'Pagos excedidos'
+msg = contenido_html
+
+mensaje = MIMEMultipart()
+
+mensaje['From'] = remitente
+mensaje['To'] = ", ".join(destinatario)
+mensaje['Subject'] = asunto
+
+mensaje.attach(MIMEText(contenido_html, 'html'))
+
+# Datos
+username = os.environ['USERNAME']
+password = os.environ['PASSWORD']
+
+# Enviando el correo
+server = smtplib.SMTP_SSL('px000056.ferozo.com:465')
+# server.starttls()
+username=remitente
+server.login(username,password)
+try:
+    server.sendmail(remitente, destinatario, mensaje.as_string())
+    server.quit()
+    print('E- mail enviado exitosamente!')
+except Exception as e:
+    print('Ha ocurrido un error:\n', e)
+
+# Cerrar el cursor y la conexión
+cursor.close()
+conexion.close()
